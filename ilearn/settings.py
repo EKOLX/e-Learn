802c0d1645ae923f9081f54b0c4e64f9cab72098
EL Khan <elkhanmursali@gmail.com>
2023-08-27 18:52:20 +0400

    Adding auth views.

--- ilearn/settings.py
+++ ilearn/settings.py
@@ -52,7 +52,7 @@ ROOT_URLCONF = "ilearn.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -112,8 +112,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

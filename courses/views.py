70978337f7c355d56f5d1465fd93b5cd6e215616
EL Khan <elkhanmursali@gmail.com>
2023-08-30 21:45:11 +0400

    Adding course detail views.

--- courses/views.py
+++ courses/views.py
@@ -1,13 +1,15 @@
 from django.views.generic.base import TemplateResponseMixin, View
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.urls import reverse_lazy
 from django.shortcuts import redirect, get_object_or_404
 from django.forms.models import modelform_factory
 from django.apps import apps
+from django.db.models import Count
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
-from .models import Course, Module, Content
+from .models import Course, Module, Content, Subject
 from .forms import ModuleFormSet
 
 
@@ -32,6 +34,26 @@ class CourseModuleUpdateView(TemplateResponseMixin, View):
             formset.save()
             return redirect("manage_course_list")
         return self.render_to_response({"course": self.course, "formset": formset})
+
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = "courses/course/list.html"
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(total_courses=Count("courses"))
+        courses = Course.objects.annotate(total_modules=Count("modules"))
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        return self.render_to_response(
+            {"subjects": subjects, "subject": subject, "courses": courses}
+        )
+
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = "courses/course/detail.html"
 
 
 class ModuleContentListView(TemplateResponseMixin, View):
